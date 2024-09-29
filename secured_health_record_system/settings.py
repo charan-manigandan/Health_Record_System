97df516cc21be27a6097be0cbe7928ac369d4ecd
CHARAN <charan.manigandan@gmail.com>
2024-09-29 21:18:55 +0530

    updated static

--- secured_health_record_system/settings.py
+++ secured_health_record_system/settings.py
@@ -138,7 +138,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
