4e3693cd698360b50d7cbeda760da5b9d041656e
CHARAN <charan.manigandan@gmail.com>
2024-09-29 12:00:04 +0530

    added collect static

--- secured_health_record_system/settings.py
+++ secured_health_record_system/settings.py
@@ -126,7 +126,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
