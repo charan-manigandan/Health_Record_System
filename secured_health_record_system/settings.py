17230fad42ce540f5521dd065b287c86db0cc7d1
CHARAN <charan.manigandan@gmail.com>
2024-10-09 23:00:46 +0530

    env read added

--- secured_health_record_system/settings.py
+++ secured_health_record_system/settings.py
@@ -21,6 +21,7 @@ env = environ.Env(
 )
 
 load_dotenv()
+environ.Env.read_env() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
