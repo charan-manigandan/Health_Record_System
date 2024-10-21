c74a7d65c5584bb0fe836ecc9c09533c79ef1555
CHARAN <charan.manigandan@gmail.com>
2024-10-21 19:00:46 +0530

    prod changes

--- secured_health_record_system/settings.py
+++ secured_health_record_system/settings.py
@@ -20,8 +20,7 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-# load_dotenv()
-environ.Env.read_env() 
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
